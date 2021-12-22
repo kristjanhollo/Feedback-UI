f11f5c9abcc0f7c76f8dc25e0d232aaca2bdb4be
kristjanhollo <hollo.kristjan@gmail.com>
2021-12-22 23:54:17 +0200

    [add] - Fade animation for delete/add

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,18 +1,37 @@
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
+import {motion, AnimatePresence} from "framer-motion";
 
 function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet!</p>;
     }
+
     return (
-        <div className="feedback-list">
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item}
-                handleDelete={handleDelete}/>
-            ))}
+        <div className='feedback-list'>
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        inital={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{opacity: 0}}
+                    >
+                        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     )
+
+    // return (
+    //     <div className="feedback-list">
+    //         {feedback.map((item) => (
+    //             <FeedbackItem key={item.id} item={item}
+    //             handleDelete={handleDelete}/>
+    //         ))}
+    //     </div>
+    // )
 }
 
 FeedbackList.propTypes = {
