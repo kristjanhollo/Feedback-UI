1963c4bbad62fbf28ee14768f5e13509ce34cadd
kristjanhollo <hollo.kristjan@gmail.com>
2021-12-22 23:21:01 +0200

    [add] - Validation for form

--- src/components/FeedBackForm.js
+++ src/components/FeedBackForm.js
@@ -5,8 +5,21 @@ import Button from './shared/Button'
 
 function FeedBackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value);
 
     }
@@ -16,8 +29,9 @@ function FeedBackForm() {
             <h2>How would you rate your service with us?</h2>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" value={text} placeholder='Write a review'/>
-                <Button type="submit" version='primary'>Send</Button>
+                <Button type="submit" version='primary' isDisabled={btnDisabled}>Send</Button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
 
