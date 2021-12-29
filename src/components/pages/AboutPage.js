1c7c611d9eb4f5eba2dfbf3d4eed5ab10fed1da8
kristjanhollo <hollo.kristjan@gmail.com>
2021-12-29 21:07:47 +0200

    [refactor] - Added github link to AboutPage.js

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -9,6 +9,7 @@ function AboutPage() {
         <h1>About this project</h1>
         <p>This is a React to leave feedback for a product or service</p>
         <p>Version: 1.0.0</p>
+        <a href='https://github.com/kristjanhollo/React'>Github</a>
         <p>
           <Link  to='/'>Back To Home</Link>
         </p>
